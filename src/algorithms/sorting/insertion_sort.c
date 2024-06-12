7241cfdbf684fe0a06f8e4d716b283fa2e345682
Brandon Wingard <56277974+AegisSSC@users.noreply.github.com>
2024-06-12 16:44:47 -0400

    initialized insertion_sort.c

---
+++ src/algorithms/sorting/insertion_sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "insertion_sort.h"
+#include <stdlib.h>
+
+
+void main(){
+    int* array = malloc(sizeof(int) * 10);
+
+
+}
+
+
+int* insertion_sort(int array)
+{
+    int * return_array;
+    return return_array;
+}
