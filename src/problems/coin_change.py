b2f81cef0d050f35d6801271be7f9293aa17af61
Brandon Wingard <wingardbrandonm@gmail.com>
2025-04-28 10:38:18 -0400

    Create coin_change.py

---
+++ src/problems/coin_change.py
@@ -0,0 +1,15 @@
+# Given three integers n, k, target, and an array of coins[] of size n. 
+# Find if it is possible to make a change of target cents by using an infinite supply of each coin but the total number of coins used must be exactly equal to k.
+
+
+# Examples:
+# Input: n = 5, k = 3, target = 11, coins = {1, 10, 5, 8, 6}
+# Output: 1
+# Explanation: 2 coins of 5 and 1 coin of 1 can be used to make a change of 11 i.e. 11 => 5 + 5 + 1.
+
+
+# Examples:
+# Input: n = 3, k = 5, target = 25, coins = {7, 2, 4}
+# Output: 1
+# Explanation: 3 coins 7, 2 coins of 2 can be used to make a change of 25 i.e. 25 => 7+7+7+2+2.
+
